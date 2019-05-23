d29601f16e6905b4157317c1fe3fe2da9ebf7961
Agata Jankowiak <xurrc.j@gmail.com>
2019-05-23 13:42:46 +0200

    Elementy gry, tło, piłka, boczki, do momentu poruszania się platformy.

---
+++ Assets/Scenes/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+
+
+    void Update ()
+    {
+
+            float mousePosInUnits = Input.mousePosition.x / Screen.width *screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        transform.position = paddlePos;
+
+    }
+}
