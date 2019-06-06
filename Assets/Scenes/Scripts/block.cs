ed057d6b5958d79d2ca0eaef13c6cdf1ebc81099
WiktoriaGawlik <wikisophie@gmail.com>
2019-06-06 14:20:37 +0200

    no message

--- Assets/Scenes/Scripts/block.cs
+++ Assets/Scenes/Scripts/block.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 
 public class block : MonoBehaviour
 {
-    ParticleSystem boom;
+    
 
     private void OnCollisionEnter2D(Collision2D collision)
 
     {
+       
         Destroy(gameObject, 0.1f);
 
-        boom.Play();
+        
     }
 
 }
---
+++ Assets/Scenes/Scripts/kurcze_nie_wiem_kolizja.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kurcze_nie_wiem_kolizja : MonoBehaviour
+{
+    public ParticleSystem boom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision);
+    {
+            boom.Play();
+    }
+}
+}
